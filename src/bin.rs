de50e3288315c751709f09d49d03e71dbc6a11b0
Connor Bulakites <connor@bulakites.net>
2021-01-02 21:14:16 -0500

    Implement Display for DecisionTree

---
+++ src/bin.rs
@@ -0,0 +1,9 @@
+extern crate wis;
+
+use wis::DecisionTree;
+
+
+fn main() {
+    let dt = DecisionTree{};
+    println!("{}", dt);
+}
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use std::fmt;
+
+
+pub struct DecisionTree {
+}
+
+
+impl fmt::Display for DecisionTree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "DecisionTree")
+    }
+}
