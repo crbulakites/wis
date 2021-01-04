d8036fb454ade9eacbb2febd3d407ddbf43418ef
Connor Bulakites <connor@bulakites.net>
2021-01-03 23:47:16 -0500

    Allow generic schema type for dataframe

--- src/bin.rs
+++ src/bin.rs
@@ -1,11 +1,19 @@
 extern crate wis;
 
 use wis::DecisionTree;
-use wis::df::*;
+use wis::df::DataFrame;
 
 
 fn main() {
-    let df = DataFrame::new(vec!["x", "y", "label"]);
+    let df = DataFrame::new(
+        vec!["x", "y", "label"],
+        vec![
+            (0.0, 0.0, false),
+            (0.0, 1.0, true),
+            (1.0, 0.0, true),
+            (1.0, 1.0, false),
+        ],
+    );
     println!("{}", df);
 
     let dt = DecisionTree{};
--- src/df.rs
+++ src/df.rs
@@ -2,25 +2,18 @@ use std::fmt;
 use std::vec::Vec;
 
 
-pub enum DataCell {
-    Number(f64),
-    Categorical(String),
-    Boolean(bool),
-}
-
-
-pub type DataRow = Vec<DataCell>;
-
-
-pub struct DataFrame {
+pub struct DataFrame<Schema> {
     pub ordered_column_names: Vec<String>,
-    pub rows: Vec<DataRow>,
+    pub rows: Vec<Schema>,
 }
 
 
-impl DataFrame {
-    pub fn new(ordered_column_names: Vec<&'static str>) -> Self {
-        let mut df = DataFrame{ordered_column_names: vec![], rows: vec![]};
+impl<Schema> DataFrame<Schema> {
+    pub fn new(
+        ordered_column_names: Vec<&'static str>,
+        rows: Vec<Schema>,
+    ) -> Self {
+        let mut df = DataFrame{ordered_column_names: vec![], rows};
 
         for column_name in ordered_column_names {
             df.ordered_column_names.push(String::from(column_name));
@@ -31,7 +24,7 @@ impl DataFrame {
 }
 
 
-impl fmt::Display for DataFrame {
+impl<Schema> fmt::Display for DataFrame<Schema> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut lines: Vec<String> = vec![];
         
